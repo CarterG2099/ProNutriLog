fd5746ac7c988385c9c0cb31f9d51504cf652e36
CarterG2099 <cgividen20@gmail.com>
2025-04-09 12:53:56 -0600

    Side effect OAUTH changes

--- app/src/main/java/com/proNutriLog/proteinCalculator/data/model/UserData.kt
+++ app/src/main/java/com/proNutriLog/proteinCalculator/data/model/UserData.kt
@@ -1,7 +1,9 @@
 package com.proNutriLog.proteinCalculator.data.model
 
 data class User(
-    val name: String = "",
+    val first_name: String = "",
+    val last_name: String = "",
+    val display_name: String = "",
     val email: String = "",
     val age: Int = 0,
     val height: Int = 0,
